561b130fec0be72805d67890355747bac50d25a9
rmit-s3607909-chithang-kwok <31905455+rmit-s3607909-chithang-kwok@users.noreply.github.com>
2018-04-23 11:33:30 +1000

    Add files via upload

---
+++ Classmate.java
@@ -0,0 +1,19 @@
+
+public class Classmate extends Person{
+	private int age;
+	private String name;
+	private String filed;
+	private String gender;
+	private String status;
+	private String state;
+	
+	public Classmate() {
+		this.age=age;
+		this.name=name;
+		this.filed=filed;
+		this.gender=gender;
+		this.status=status;
+		this.state=state;
+	}
+
+}
---
+++ Collegues.java
@@ -0,0 +1,19 @@
+
+public class Collegues extends Person{
+	private int age;
+	private String name;
+	private String filed;
+	private String gender;
+	private String status;
+	private String state;
+	
+	public Collegues() {
+		this.age=age;
+		this.name=name;
+		this.filed=filed;
+		this.gender=gender;
+		this.status=status;
+		this.state=state;
+
+}
+}
