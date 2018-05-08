5bcfb78f20a2be15af16663a0a9868b9aff33313
rmit-s3674270-wenpeng-jiang <31913846+rmit-s3674270-wenpeng-jiang@users.noreply.github.com>
2018-05-08 11:18:12 +1000

    Update Child.java

--- Child.java
+++ Child.java
@@ -1,29 +1,78 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
 
-public class Child extends Person{
-	private int age;
-	private String name;
-	private String filed;
-	private String gender;
-	private String status;
-	private String state;
+
+public class Child extends Person {
+	List<Child> friendslist = new ArrayList<Child>();
+	List<Adult> parentslist = new ArrayList<Adult>();
+	List<Person> classmatelist = new ArrayList<Person>();
 	
-	//Author: JIANG
-    //We define teenager as type of a person and they can be friends between 2 to 16 years old.
-	public Child() {
-		this.age=age;
-		this.name=name;
-		this.filed=filed;
-		this.gender=gender;
-		this.status=status;
-		this.state=state;
-    }
+	public class NoParentException extends Exception{}
+	public class NotToBeFriendsException extends Exception{}
+	public class TooYoungException extends Exception{}
+	public class NotToBeClassmateException extends Exception{}
+	
+	
+	public void connectfriends(Object obj) throws Exception{
+		try {
+			if (obj instanceof Adult) {
+				throw new NotToBeFriendsException();
+			}
+			if (obj instanceof YoungChild) {
+				throw new TooYoungException();
+			}
+
+			if (obj instanceof Child) {
+				Child c = (Child) obj;
+				if(this.getAge() - c.getAge() > 3 || c.getAge() - this.getAge() > 3) {
+					throw new NotToBeFriendsException();
+				}
+				else {
+					this.friendslist.add(c);
+											//add back
+				}
+			}
+			
+		}catch (NotToBeFriendsException e) {
+			System.out.println("Cannot make friends with an adult or child with an age gap larger than 3");
+		}catch (TooYoungException e) {
+			System.out.println("Cannot make friends with a young child");
+		}
+	}
+	
+	public void connectparents(Object obj) {	
+		if (this.parentslist.size() == 2) {
+			System.out.println("Fail to add parents");
+			return;
+		}
+		else {
+			if (obj instanceof Adult) {
+				Adult a = (Adult) obj;
+				System.out.println("You have added");
+				this.parentslist.add(a);
+			}
+			else {	
+				return;
+			}
+		}	
+	}
+	
+	public void connectclassmates(Object obj) throws Exception{
+		try {
+			if (obj instanceof YoungChild) {
+				throw new NotToBeClassmateException();
+			}
+			else {
+				Person p = (Person) obj;
+				this.classmatelist.add(p);
+			}
+		}catch (NotToBeClassmateException e) {
+			System.out.println("Cannot connect a young child in a classmate relation");
+		}
+	}
+	
+	public void indentify(Object obj) {
+		
+	}
+	
 }
-	
-	
-
-
-
