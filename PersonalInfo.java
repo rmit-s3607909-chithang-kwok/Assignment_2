b92441c1386c8d5d983fa5de2384eac9ce1166e3
rmit-s3607909-chithang-kwok <31905455+rmit-s3607909-chithang-kwok@users.noreply.github.com>
2018-05-16 18:13:51 +1000

    Add files via upload

--- PersonalInfo.java
+++ PersonalInfo.java
@@ -10,6 +10,7 @@ import java.util.regex.Pattern;
 
 import com.sun.javafx.scene.control.skin.IntegerFieldSkin;
 
+import javafx.beans.binding.IntegerBinding;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -48,7 +49,7 @@ import javafx.util.converter.NumberStringConverter;
 public class PersonalInfo implements Initializable {
 
     //configure the table
-     private TableView<Person> tableView;
+     private TableView<Person> table;
      private TableColumn<Person, String> NameColumn;
      private TableColumn<Person, Image>  ImageColumn;
      private TableColumn<Person, String> StatusColumn;
@@ -61,7 +62,7 @@ public class PersonalInfo implements Initializable {
      private TextField ImageTextField;
      private TextField StatusTextField;
      private TextField GenderTextField;
-     private TextFormatter AgeTextField;
+     private TextField AgeTextField;
      private TextField StateTextField;
     
      private Button detailedPersonViewButton;
@@ -72,7 +73,7 @@ public class PersonalInfo implements Initializable {
     
         
 
-     
+    
      
     /**
      * This method will allow the user to double click on a cell and update
@@ -80,7 +81,7 @@ public class PersonalInfo implements Initializable {
      */
     public void changeNameCellEvent(CellEditEvent edittedCell)
     {
-        Person personSelected =  tableView.getSelectionModel().getSelectedItem();
+        Person personSelected =  table.getSelectionModel().getSelectedItem();
         personSelected.setName(edittedCell.getNewValue().toString());
     }
     
@@ -90,7 +91,7 @@ public class PersonalInfo implements Initializable {
      */
     public void changeStatusCellEvent(CellEditEvent edittedCell)
     {
-        Person personSelected =  tableView.getSelectionModel().getSelectedItem();
+        Person personSelected =  table.getSelectionModel().getSelectedItem();
         personSelected.setStatus(edittedCell.getNewValue().toString());
     }
     
@@ -100,7 +101,7 @@ public class PersonalInfo implements Initializable {
      */
     public void changeStateCellEvent(CellEditEvent edittedCell)
     {
-        Person personSelected =  tableView.getSelectionModel().getSelectedItem();
+        Person personSelected =  table.getSelectionModel().getSelectedItem();
         personSelected.setState(edittedCell.getNewValue().toString());
     }
     
@@ -113,8 +114,8 @@ public class PersonalInfo implements Initializable {
     	
     	
     	
-        Person personSelected =  tableView.getSelectionModel().getSelectedItem();
-        personSelected.setAge(edittedCell.getNewValue().toString());
+        Person personSelected =  table.getSelectionModel().getSelectedItem();
+        personSelected.setAge(Integer.parseInt(edittedCell.getNewValue().toString()));
    }
     
     /**
@@ -123,7 +124,7 @@ public class PersonalInfo implements Initializable {
      */
     public void changeGenderCellEvent(CellEditEvent edittedCell)
     {
-        Person personSelected =  tableView.getSelectionModel().getSelectedItem();
+        Person personSelected =  table.getSelectionModel().getSelectedItem();
         personSelected.setGender(edittedCell.getNewValue().toString());
     }
     /**
@@ -140,13 +141,19 @@ public class PersonalInfo implements Initializable {
      * When this method is called, it will change the Scene to 
      * a SubMenu
      */
+    
+   
     public void changeScreenButtonPushed(ActionEvent event) throws IOException
     {
+    	
+    	
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("SubMenu.fxml"));
         Scene tableViewScene = new Scene(tableViewParent);
         
         //This line gets the Stage information
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        
+       
         
         window.setScene(tableViewScene);
         window.show();
@@ -156,8 +163,12 @@ public class PersonalInfo implements Initializable {
      * When this method is called, it will pass the selected Person object to
      * a the detailed view
      */
+  
+    
     public void changeSceneToDetailedPersonView(ActionEvent event) throws IOException
     {
+    	
+		
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("PersonDetails.fxml"));
         Parent tableViewParent = loader.load();
@@ -165,63 +176,62 @@ public class PersonalInfo implements Initializable {
         Scene tableViewScene = new Scene(tableViewParent);
         
         //access the controller and call a method
-        PersonDetails controller = loader.getController();
-        controller.initData(tableView.getSelectionModel().getSelectedItem());
+       PersonDetails controller = loader.getController();
+       controller.initData(table.getSelectionModel().getSelectedItem());
         
         //This line gets the Stage information
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
         
         window.setScene(tableViewScene);
         window.show();
+       
     }
-    
-  
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        //set up the columns in the table
+    	
         NameColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Name"));
-       // ImageColumn.setCellValueFactory(new PropertyValueFactory<Person, Image>("Image"));
-        StatusColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Status"));
-        GenderColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Gender"));
-        AgeColumn.setCellValueFactory(new PropertyValueFactory<Person, Integer>("Age"));
-        StateColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("State"));
+        //ImageColumn.setCellValueFactory(new PropertyValueFactory<Person, Image>("Image"));
+       StatusColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Status"));
+       GenderColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("Gender"));
+       AgeColumn.setCellValueFactory(new PropertyValueFactory<Person, Integer>("Age"));
+       StateColumn.setCellValueFactory(new PropertyValueFactory<Person, String>("State"));
         
         //load dummy data
-        tableView.setItems(getPeople());
+        table.setItems(getPerson());
         
         //Update the table to allow for the fields
         //to be editable
-        tableView.setEditable(true);
+        table.setEditable(true);
         NameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
        // ImageColumn.setCellFactory(TextFieldTableCell.forTableColumn());
         StatusColumn.setCellFactory(TextFieldTableCell.forTableColumn());
         GenderColumn.setCellFactory(TextFieldTableCell.forTableColumn());
-        AgeColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+       AgeColumn.setCellFactory(TextFieldTableCell.forTableColumn(new IntegerStringConverter()));
+        
         StateColumn.setCellFactory(TextFieldTableCell.forTableColumn());
         
         
         //This will allow the table to select multiple rows at once
-        tableView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        table.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
         
         //Disable the detailed person view button until a row is selected
         this.detailedPersonViewButton.setDisable(true);
     }    
     
     
-    /**
-     * This method will remove the selected row(s) from the table 
-     */
+
+    
     public void deleteButtonPushed()
     {
         ObservableList<Person> selectedRows, allPeople;
-        allPeople = tableView.getItems();
+        allPeople = table.getItems();
         
         //this gives us the rows that were selected
-        selectedRows = tableView.getSelectionModel().getSelectedItems();
+       selectedRows = table.getSelectionModel().getSelectedItems();
         
         //loop over the selected rows and remove the Person objects from the table
         for (Person person: selectedRows)
@@ -232,42 +242,36 @@ public class PersonalInfo implements Initializable {
     
     
     
-    /**
-     * This method will create a new Person object and add it to the table
-     */
-    public void newPersonButtonPushed()
-    {
-        
+    
+   public void newPersonButtonPushed() {
 		Person newPerson = new Person(NameTextField.getText(),
                                       StatusTextField.getText(),
-                                      GenderTextField.getText(),
-                                      AgeTextField.getText(),
+                                     GenderTextField.getText(),
+                                     Integer.parseInt(AgeTextField.getText()),
                                       StateTextField.getText());
         
-        //Get all the items from the table as a list, then add the new person to
+      //  Get all the items from the table as a list, then add the new person to
         //the list
-        tableView.getItems().add(newPerson);
+        table.getItems().add(newPerson);
     }
     
     
     
-    /**
-     * This method will return an ObservableList of People objects
-     */
-    public ObservableList<Person>  getPeople()
-    {
-        ObservableList<Person> people = FXCollections.observableArrayList();
-        people.add(new Person("Alex Smith",new Image("AlexSmith.jpg"), "student at RMIT", "M", 35, "WA" ));
-        people.add(new Person("Ben Turner",new Image("BenTurner.jpg"), "manager at ZFX", "M", 45, "VIC" ));
-        people.add(new Person("Hannah White",new Image("HannahWhite.jpg"), "student at PLC", "F", 12, "VIC" ));
-        people.add(new Person("Zoe Foster",new Image("Zoe Foster.jpg"), "Founder of ZFX", "M", 33, "VIC" ));
-        people.add(new Person("Mary Turner",new Image("MaryTurner.jpg"), "looking for job", "F", 2, "VIC" ));
-        people.add(new Person("john Smith",new Image("JohnSmith.jpg"), "student at PLC", "M", 12, "VIC" ));
-        people.add(new Person("Mary Hopkin",new Image("MaryHopkin.jpg"), "House Worker", "F", 38, "TAS" ));
-        people.add(new Person("Alex Turner",new Image("AlexTurner.jpg"), "student at LaTrobe", "M", 22, "VIC" ));
-        people.add(new Person("Klay Thomas",new Image("KlayThomas.jpg"), "House Worker", "F", 33, "NSW" ));
-        
-        
-        return people;
-    }
+   
+	//Get all of the Person
+	public ObservableList<Person> getPerson(){
+    ObservableList<Person> Person = FXCollections.observableArrayList();
+    Person.add(new Person("Alex Smith", "student at RMIT", "M", 35, "WA" ));
+    Person.add(new Person("Ben Turner", "manager at ZFX", "M", 45, "VIC" ));
+    Person.add(new Person("Hannah White", "student at PLC", "F", 12, "VIC" ));
+    Person.add(new Person("Zoe Foster", "Founder of ZFX", "M", 33, "VIC" ));
+    Person.add(new Person("Mary Turner", "looking for job", "F", 2, "VIC" ));
+    Person.add(new Person("john Smith", "student at PLC", "M", 12, "VIC" ));
+    Person.add(new Person("Mary Hopkin", "House Worker", "F", 38, "TAS" ));
+    Person.add(new Person("Alex Turner", "student at LaTrobe", "M", 22, "VIC" ));
+    Person.add(new Person("Klay Thomas", "House Worker", "F", 33, "NSW" ));
+            
+    return Person;
 }
+}
+
